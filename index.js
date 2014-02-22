55d2fed8d4d90b36e37ecbfb501828f145156783
angleman <angleman@cormea.com>
2014-02-21 18:10:12 -0700

    package relative fix

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var module_path = 'app_modules';
 
-function aquire(moduleName, setPath) {
+module.exports = function(moduleName, setPath) {
 	if (setPath) { module_path = setPath }
 	if (['bower', 'component', 'config', 'package'].indexOf(moduleName) >= 0) {
-		return require('./' + moduleName + '.json')
+		return require('../../' + moduleName + '.json')
 	}
-	return require('./' + module_path + '/' + moduleName)
+	var path = (module_path.substr(0,1) == '/') ? module_path : '../../' + module_path;
+	return require(path + '/' + moduleName)
 }
-
-module.exports = aquire
