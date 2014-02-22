64ca6bddf0196ec6acdedbde78b8d6829fa37527
angleman <angleman@cormea.com>
2014-02-21 17:45:53 -0700

    path fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var module_path = 'app_modules';
 function aquire(moduleName, setPath) {
 	if (setPath) { module_path = setPath }
 	if (['bower', 'component', 'config', 'package'].indexOf(moduleName) >= 0) {
-		return require('../' + moduleName + '.json')
+		return require('./' + moduleName + '.json')
 	}
-	return require('../' + module_path + '/' + moduleName)
+	return require('./' + module_path + '/' + moduleName)
 }
 
 module.exports = aquire
